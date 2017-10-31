d0e567b2222b1ae24d7dca246eaa268ca773df0c
Tiffany Han <hantiffany@gmail.com>
2017-10-31 10:48:07 -0700

    add bumpScore functionality

--- app.js
+++ app.js
@@ -1,35 +1,11 @@
 const redis = require("redis");
+// TODO: make createClient its own method, in order
+// to pass in custom config options
 const client = redis.createClient();
 
-// client.zadd('he', 1, 'hello');
-// client.zincrby('he', 5, 'hello'); // 6
-// client.zscore('he', 'hell', (err, reply) => {
-//   console.log(reply)
-//   console.log(err)
-// });
-
-
-// import
-// input: take a collection of strings ("completions")
-// output: number of items inserted
-// side effect: index data into redis
-// needs to process each completion
-
-// for each completion
-// subproblem: how to get all the prefixes of a string
-// input: a string
-// output: array of prefix substrings
-// loop from 1 through length of string
-// always slice 0 through i
-
-// adding a completion to redis
-// input: array of prefix strings
-// output: none
-// sideffect:
-//   for each prefix
-//   zadd prefix 0 completion
-
 // later: configuration option - include full string
+// also clean up completions before importing
+// E.g. normalize case and whitespaces
 
 const batchImport = completions => {
   completions.forEach(completion => {
@@ -67,6 +43,20 @@ const search = prefixQuery => {
   ));
 };
 
-search("h");
+// search("h");
+
+// we increment by -1, bc this enables us to sort
+// by frequency plus ascending lexographical order in Redis
+const bumpScore = completion => {
+  const prefixes = extractPrefixes(completion);
+  prefixes.forEach(prefix => (
+    client.zincrby(prefix, -1, completion)
+  ));
+  return;
+};
+
+bumpScore("walid");
+bumpScore("waldo");
+bumpScore("waldo");
 
 client.quit();
--- notes.md
+++ notes.md
@@ -4,3 +4,37 @@ test early
   test all the time actually
 
 "premature optimization is the root of all evil"
+
+// client.zadd('he', 1, 'hello');
+// client.zincrby('he', 5, 'hello'); // 6
+// client.zscore('he', 'hell', (err, reply) => {
+//   console.log(reply)
+//   console.log(err)
+// });
+
+// import
+// input: take a collection of strings ("completions")
+// output: number of items inserted
+// side effect: index data into redis
+// needs to process each completion
+
+// for each completion
+// subproblem: how to get all the prefixes of a string
+// input: a string
+// output: array of prefix substrings
+// loop from 1 through length of string
+// always slice 0 through i
+
+// adding a completion to redis
+// input: array of prefix strings
+// output: none
+// sideffect:
+//   for each prefix
+//   zadd prefix 0 completion
+
+// input: completion
+// output: return number of prefixes updated
+// side effect: increment the score of the completion
+
+// for every prefix of the completion,
+//   we increment by 1 the completion in its bucket
