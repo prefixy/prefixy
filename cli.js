3eddb2532cbb1e000fdafb91e595d75933625083
Tiffany Han <hantiffany@gmail.com>
2017-10-31 18:58:08 -0700

    create a prefixy command line tool

--- cli.js
+++ cli.js
@@ -1,1 +1,23 @@
+#!/usr/bin/env node
+
 const App = require("./app");
+const program = require("commander");
+
+program
+  .version('0.0.1')
+  .description(`Prefixy: a prefix hash trie that utilizes
+    the skip list probabilistic data structure`);
+
+program
+  .command('ping')
+  .action(() => console.log('PONG'));
+
+program
+  .command('search <prefixQuery>')
+  .action((prefixQuery) => (
+    App.search(prefixQuery)
+  ));
+
+program.parse(process.argv);
+
+App.client.quit();
