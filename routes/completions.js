a721c537d0c91b6fefafa39baddcf947b5ebb300
Jay Shenk <jay.shenk@gmail.com>
2017-11-05 22:49:35 -0500

    add GET /completions and PUT /dynamic-increment endpoints

--- routes/completions.js
+++ routes/completions.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-const Prefixy = require(path.resolve(path.dirname(__dirname), 'prefixy');
+const _ = require('lodash');
+const Prefixy = require(path.resolve(path.dirname(__dirname), 'prefixy'));
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  const prefix = JSON.parse(req.body);
-  console.log(prefix);
-  res.send("");
+const formatCompletionsWithScores = completions => {
+  return _.chunk(completions, 2).map(completion => (
+    {
+      completion: completion[0],
+      score: completion[1]
+    }
+  ));
+};
+
+router.get('/', async function(req, res) {
+  const prefix = req.body.prefix;
+  const opts = {
+    limit: req.body.limit || 5,
+    withScores: req.body.withScores || false,
+  };
+  let completions = await Prefixy.search(prefix, opts);
+
+  if (opts.withScores) {
+    completions = formatCompletionsWithScores(completions);
+  }
+
+  res.json(completions);
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+const Prefixy = require(path.resolve(path.dirname(__dirname), 'prefixy'));
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send("hello world");
+router.put('/dynamic-increment', async function(req, res) {
+  const completion = req.body.completion;
+  const scores = await Prefixy.dynamicIncrementScore(completion);
+
+  res.json({ completion, score: scores[0] });
 });
 
 module.exports = router;
