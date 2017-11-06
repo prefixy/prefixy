9635758037e69e0a5de1dceee164ee24af7f6855
Walid Wahed <wwahed1@gmail.com>
2017-11-06 13:23:17 -0500

    POST, DELETE route refactor

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
-var completions = require('./routes/completions');
 
 var app = express();
 
@@ -24,8 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
-app.use('/completions', completions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
