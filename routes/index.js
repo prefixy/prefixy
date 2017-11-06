08ada2b99792827d1973f016457b9ef22c201d2e
Jay Shenk <jay.shenk@gmail.com>
2017-11-06 13:31:06 -0500

    move /completions to index route file

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+const _ = require('lodash');
 const Prefixy = require(path.resolve(path.dirname(__dirname), 'prefixy'));
+
+const formatCompletionsWithScores = completions => {
+  return _.chunk(completions, 2).map(completion => (
+    {
+      completion: completion[0],
+      score: completion[1]
+    }
+  ));
+};
+
+router.get('/completions', async function(req, res) {
+  const prefix = req.body.prefix;
+  const opts = {
+    limit: req.body.limit || 5,
+    withScores: req.body.withScores || false,
+  };
+  let completions = await Prefixy.search(prefix, opts);
+
+  if (opts.withScores) {
+    completions = formatCompletionsWithScores(completions);
+  }
+
+  res.json(completions);
+});
 
 router.put('/dynamic-increment', async function(req, res) {
   const completion = req.body.completion;
