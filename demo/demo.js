edbd5eca79a53d8b58bd35416cf383c782ba83a6
Tiffany Han <hantiffany@gmail.com>
2017-11-07 13:03:59 -0800

    create a binding so that submitting a completion PUTs to the server

--- demo/demo.js
+++ demo/demo.js
@@ -1,11 +1,25 @@
+var form = document.querySelector('form');
+
+const submitCompletion = (e) => {
+  e.preventDefault();
+
+  const completion = form.querySelector('input[type=text]').value;
+  axios.put("http://localhost:3000/dynamic-increment", { completion });
+  // axios.put("http://localhost:3000/increment", { completion });
+};
+
 var my_autoComplete = new autoComplete({
-  selector: 'input',
+  selector: 'input[type=text]',
   minChars: 1,
+  cache: false,
   source: function(term, suggest) {
     axios.get("http://localhost:3000/completions", {
       params: {
         prefix: term
       }
     }).then((response) => suggest(response.data))
-  }
+  },
+  onSelect: submitCompletion,
 });
+
+form.addEventListener('submit', submitCompletion);
--- seed.js
+++ seed.js
@@ -1,30 +1,30 @@
 const path = require("path");
-const App = require(path.resolve(__dirname, "prefixy"));
+const Prefixy = require(path.resolve(__dirname, "prefixy"));
 
 const funNames = ['jay', 'tiffany', 'walid', 'kevin', 'waldo', 'wally', 'walden', 'jays', 'jacqueline', 'jay', 'jones', 'jay jay', 'homer jay simpson', 'tin', 'tim', 'timbuktu', 'till', 'true'];
 
-App.insertCompletions(funNames);
+Prefixy.insertCompletions(funNames);
 
-App.bumpScore("walid");
-App.bumpScore("waldo");
-App.bumpScore("waldo");
+Prefixy.fixedIncrementScore("walid");
+Prefixy.fixedIncrementScore("waldo");
+Prefixy.fixedIncrementScore("waldo");
 
-App.setScore("walter", -500);
+Prefixy.insertCompletions([{completion: "walter", score: 500}]);
 
 const sample = [
-  { completion: "mary", score: -20 },
-  { completion: "poppins", score: -5 },
-  { completion: "walmart", score: -10 },
+  { completion: "mary", score: 20 },
+  { completion: "poppins", score: 5 },
+  { completion: "walmart", score: 10 },
   { completion: "wario", score: 0 },
-  { completion: "waluigi", score: -1 },
-  { completion: "javascript", score: -99 },
-  { completion: "reddit", score: -44 },
-  { completion: "hackernews", score: -2 },
-  { completion: "chris lee", score: -20 },
-  { completion: "nascar", score: -20 },
-  { completion: "walid", score: -1000 },
+  { completion: "waluigi", score: 1 },
+  { completion: "javascript", score: 99 },
+  { completion: "reddit", score: 44 },
+  { completion: "hackernews", score: 2 },
+  { completion: "chris lee", score: 20 },
+  { completion: "nascar", score: 20 },
+  { completion: "walid", score: 1000 },
 ]
 
-// App.insertCompletionsWithScores(sample);
+Prefixy.insertCompletions(sample);
 
-App.client.quit();
+Prefixy.client.quit();
