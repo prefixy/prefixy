f26be6e3635e4f32b573afa959a46d2fe77e0cfd
Walid Wahed <wwahed1@gmail.com>
2017-11-16 16:37:53 -0500

    add favicon

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const index = require('./routes/index');
 const app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
